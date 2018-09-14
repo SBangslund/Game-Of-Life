3270e41dcd5da800337c097cbb523e326ade7988
Samuel Bangslund <s.bangslund@hotmail.dk>
2018-09-14 22:13:35 +0200

    Initial commit

---
+++ src/gameoflife/Cell.java
@@ -0,0 +1,177 @@
+package gameoflife;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+/**
+ * @author Samuel Bangslund
+ *
+ * The cell class. This class is meant to check and control each of the cells
+ * individually.
+ */
+public class Cell {
+
+    int boxWidth    = 5;    // Width of the cell.
+    int boxHeight   = 5;    // Height of the cell.
+    int xPos        = 0;    // The xPosition of the cell. (In px)
+    int yPos        = 0;    // The yPosition of the cell. (In px)
+
+    boolean markedFilled = false;   // Whether or not the cell is marked.
+    boolean isFilled = false;       // Whether or not the cell is filled.
+
+    Canvas canvas;  // Canvas for global access.
+
+    /**
+     * The cell object.  
+     * @param width of the cell.
+     * @param height of the cell.
+     * @param x position of the cell. (In px)
+     * @param y position of the cell. (In px)
+     */
+    public Cell(int width, int height, int x, int y) {
+        setWidth(width);
+        setHeight(height);
+
+        setXPos(x);
+        setYPos(y);
+
+        canvas = GameOfLife.canvas; // Creates a reference to the main class.
+    }
+
+    /**
+     * Apply all the rules for the cell. This is based on the
+     * number of neighbours counted in the countNeighbours() method.
+     * @param gc the GraphicsContext object.
+     */
+    public void checkCell(GraphicsContext gc) {
+        int neighbours = countNeighbours(); // Gets the number of neighbours.
+
+        if (neighbours < 2) {                           // rule 1
+            markForEmpty();
+        } else if (neighbours == 3) {                   // rule 2
+            markForFill();
+        } else if (neighbours == 2 || neighbours == 3) {// rule 3
+            if (isFilled) {
+                markForFill();
+            } else {
+                markForEmpty();
+            }
+        } else if (neighbours > 3) {                    // rule 4
+            markForEmpty();
+        }
+    }
+
+    /**
+     * Converts the cell depending on if the cell is marked for fill or not.
+     * @param gc the GraphicsContext object.
+     */
+    public void convertCell(GraphicsContext gc) {
+        if (markedFilled) {
+            fillCell(gc);
+        } else {
+            emptyCell(gc);
+        }
+    }
+
+    /**
+     * Counts the number of neighbours around the cell. (3x3)
+     * @return number of neighbours.
+     */
+    public int countNeighbours() {
+        int totalNeighbours = 0;
+
+        // Iterates through the 3x3 area around the cell.
+        for (int y = getLocalY() - 1; y <= getLocalY() + 1; y++) {
+            for (int x = getLocalX() - 1; x <= getLocalX() + 1; x++) {
+                // Ignore if the cell to check is itself.
+                if (y == getLocalY() && x == getLocalX()) {
+                
+                // Ignore if the cell to check is out of limits.
+                } else if (y < 0 || y > canvas.getHeight() / boxHeight - 1 || x < 0 || x > canvas.getWidth() / boxWidth - 1) {
+                } else {
+                    // Increments the number of neighbours if a live cell is
+                    // found among the checked cells.
+                    if (GameOfLife.cells[y][x].isFilled) {
+                        totalNeighbours++;
+                    }
+                }
+            }
+        }
+        return totalNeighbours;
+    }
+
+    /**
+     * Mark the cell for fill.
+     */
+    public void markForFill() {
+        markedFilled = true;
+    }
+
+    /**
+     * Mark the cell for empty.
+     */
+    public void markForEmpty() {
+        markedFilled = false;
+    }
+
+    /**
+     * Fill the cell.
+     * @param gc the GraphicContext object.
+     */
+    public void fillCell(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.fillRect(getGlobalX(), getGlobalY(), getWidth(), getHeight());
+
+        isFilled = true;
+    }
+
+    /**
+     * Empties the cell.
+     * @param gc the GraphicsContext object.
+     */
+    public void emptyCell(GraphicsContext gc) {
+        gc.setFill(Color.WHITE);
+        gc.fillRect(getGlobalX(), getGlobalY(), getWidth(), getHeight());
+
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(0.1);
+        gc.strokeRect(getGlobalX(), getGlobalY(), getWidth(), getHeight());
+
+        isFilled = false;
+    }
+
+    // Setters
+    private void setWidth(int width) {
+        boxWidth = (width > 2) ? width : boxWidth;
+    }
+    private void setHeight(int height) {
+        boxHeight = (height > 2) ? height : boxHeight;
+    }
+    private void setXPos(int x) {
+        xPos = x;
+    }
+    private void setYPos(int y) {
+        yPos = y;
+    }
+
+    // Getters
+    int getWidth() {
+        return boxWidth;
+    }
+    int getHeight() {
+        return boxHeight;
+    }
+    int getGlobalX() {
+        return xPos;
+    }
+    int getGlobalY() {
+        return yPos;
+    }
+    int getLocalX() {
+        return xPos / boxWidth;
+    }
+    int getLocalY() {
+        return yPos / boxHeight;
+    }
+}
---
+++ src/gameoflife/GameOfLife.java
@@ -0,0 +1,234 @@
+package gameoflife;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+/**
+ * @author Samuel Bangslund
+ * 
+ * Game of life. 
+ * The concept is further described on the Wikipage: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+ * 
+ * This is the main class to run the game - which is run on the JavaFX application.
+ */
+public class GameOfLife extends Application {
+
+    static Cell[][] cells;  // Where we will store all the cells as a grid.
+
+    int count           = 0;    // Used for the delay between each generation.
+    int cellWidth       = 10;   // Width of the cells.
+    int cellHeight      = 10;   // Height of the cells.
+    int generationSpeed = 8;    // The time between each generation. (Depends on the system speed)
+
+    static boolean running = false; // Whether or not the program should continue on to the next generation.
+
+    static Group root;          // A JavaFX group component - used when drawing on the canvas.
+    static Scene scene;         // A JavaFX scene component - what scene we are currently drawing on.
+    static Canvas canvas;       // A JavaFX canvas component - where to draw.
+    static GraphicsContext gc;  // A JavaFX class for drawing on the canvas.
+
+    Button toggleButton;    // The toggle button. Toggle the simulation on and off.
+    Button resetButton;     // The reset button. Resets the cells to empty.
+
+    
+    @Override
+    /**
+     * This is where the JavaFX application "starts".
+     * From here the program is initialized and displayed.
+     */
+    public void start(Stage stage) {
+        stage.setTitle("Game of life");     // Sets the title of the window.
+
+        // Create buttons for controlling the simulation.
+        toggleButton = new Button("Toggle");// Creates a button with the label: Toggle
+        resetButton = new Button("Reset");  // Creates a button with the label: Reset
+        resetButton.setTranslateX(55);      // The reset button should not be on top of the other button.
+
+        // Create necessary JavaFX components for the simulation.
+        root = new Group();             // The group of where we will store our components.
+        canvas = new Canvas(1000, 600); // The canvas to draw on. This will also act as the window.
+        gc = canvas.getGraphicsContext2D(); // A class to draw shapes.
+
+        // Create the cell array - with a length based on the
+        // window resolution and the cell size (width and height)
+        cells = new Cell[(int) (canvas.getHeight() / cellWidth)][(int) (canvas.getWidth() / cellHeight)];
+
+        
+        drawGrid();   // Draw the initial grid.
+        setup();      // Setup eventhandlers.
+        drawLoop();   // Run the main loop for the simulation.
+
+        // Add all the components to the group.
+        root.getChildren().add(canvas);
+        root.getChildren().add(toggleButton);
+        root.getChildren().add(resetButton);
+
+        // Add the group to a scene and show that scene.
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    /**
+     * Creates the necessary eventHandlers for the simulation to be controlled.
+     * This include actionEvents for the buttons and mouseEvent for mouse input.
+     */
+    void setup() {
+        // Creates the ActionEvent for the toggle button. 
+        // The toggle button proceeds to toggle between running and
+        // not running. Starting and stopping the simulation.
+        toggleButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                running = !running;
+            }
+        });
+
+        // Creates the ActionEvent for the reset button.
+        // The reset button will reset all the cells to their empty state.
+        resetButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                resetCells();
+            }
+        });
+
+        // Creates the MouseEvent for the mouse. 
+        // This awaits mouse input and upon clicking, will convert
+        // the mouse coordinates to cell coordinates. The cell will then
+        // be filled or emptied depending on what state it was at first.
+        canvas.addEventHandler(MouseEvent.MOUSE_CLICKED,
+                new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent t) {
+                if (t.getClickCount() > 0) {
+                    Cell cell = cells[(int) Math.floor(t.getY() / cellWidth)][(int) Math.floor(t.getX() / cellHeight)];
+
+                    if (cell.isFilled) {
+                        emptyDot(cell);
+                    } else {
+                        drawDot(cell);
+                    }
+                }
+            }
+        });
+    }
+
+    /**
+     * Draws the initial grid for the cells. This also assigns all the
+     * cells to the cells array - with the cell class properties.
+     */
+    void drawGrid() {
+        gc.setStroke(Color.BLACK);  // Makes sure the borders of the cells are BLACK.
+        gc.setLineWidth(0.1);       // Makes sure the width of the border is 0.1 wide.
+
+        // Iterates through all the cell coordinates and then proceeds to draw
+        // a box around the cell, effectively creating a grid. After each draw,
+        // the cell is then added to the cells[][] array.
+        for (int y = 0; y < canvas.getHeight() / cellHeight; y++) {
+            for (int x = 0; x < canvas.getWidth() / cellWidth; x++) {
+                gc.strokeRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
+                cells[y][x] = new Cell(cellWidth, cellHeight, x * cellWidth, y * cellHeight);
+            }
+        }
+    }
+
+    /**
+     * Fills in the given box.
+     * @param cell to fill in.
+     */
+    void drawDot(Cell cell) {
+        cell.fillCell(gc);  // Fill the cell.
+        gc.restore();       // Update.
+    }
+
+    /**
+     * Empties the given box.
+     * @param cell to empty.
+     */
+    void emptyDot(Cell cell) {
+        cell.emptyCell(gc); // Empty the cell.
+        gc.restore();       // Update.
+    }
+
+    /**
+     * Converts the cells depending on how the check went.
+     * If the cell is marked for filled, the cell will be filled.
+     * If the cell isn't marked for filled, the cell will be emptied.
+     */
+    void convertCells() {
+        for (int y = 0; y < canvas.getHeight() / cellHeight; y++) {
+            for (int x = 0; x < canvas.getWidth() / cellWidth; x++) {
+                Cell cell = cells[y][x];
+
+                if (cell.markedFilled) {
+                    cell.fillCell(gc);
+                } else {
+                    cell.emptyCell(gc);
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks all the cells by applying the rules of the "Game of life".
+     * (The cells have their own check)
+     */
+    void checkCells() {
+        for (int y = 0; y < canvas.getHeight() / cellHeight; y++) {
+            for (int x = 0; x < canvas.getWidth() / cellWidth; x++) {
+                cells[y][x].checkCell(gc);
+            }
+        }
+    }
+
+    /**
+     * Resets all the cells to the empty state.
+     * (Marks all the cells for empty and then runs the convertCells() method)
+     */
+    void resetCells() {
+        for (int y = 0; y < canvas.getHeight() / cellHeight; y++) {
+            for (int x = 0; x < canvas.getWidth() / cellWidth; x++) {
+                cells[y][x].markForEmpty();
+            }
+        }
+        convertCells();
+    }
+
+    /**
+     * The main loop of the program. From here all the cells are
+     * checked for changes and then converted depending on their markings.
+     */
+    void drawLoop() {
+        new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (running) {
+                    if(count % generationSpeed == 0) {
+                        checkCells();
+                        convertCells();
+
+                        gc.restore();
+                    }
+                    count++;
+                }
+            }
+        }.start();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
